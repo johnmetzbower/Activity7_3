11ab9fd8cd96907bd10ff3e62adb5b4d151041cd
johnmetzbower <johnmetzbower@nokware.net>
2014-12-11 13:27:12 -0600

    Activity7_3

---
+++ Activity7_3/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity7_3
+{
+    public abstract class Account
+    {
+        private int _accountNumber;
+
+        public int AccountNumber
+        {
+            get { return _accountNumber; }
+            set { _accountNumber = value; }
+        }
+
+        public abstract string GetAccountInfo();
+    }
+    public class CheckingAccount : Account
+    {
+        public override string GetAccountInfo()
+        {
+            return "Printing checking account info for account number "
+                + AccountNumber.ToString();
+        }
+    }
+    public class SavingsAccount : Account
+    {
+        public override string GetAccountInfo()
+        {
+            return "Printing savings account info for account number "
+                + AccountNumber.ToString();
+        }
+    }
+}
---
+++ Activity7_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> AccountList = new List<Account>();
+            CheckingAccount oCheckingAccount = new CheckingAccount();
+            oCheckingAccount.AccountNumber = 100;
+            SavingsAccount oSavingsAccount = new SavingsAccount();
+            oSavingsAccount.AccountNumber = 200;
+            AccountList.Add(oCheckingAccount);
+            AccountList.Add(oSavingsAccount);
+            
+            foreach (Account a in AccountList)
+            {
+                Console.WriteLine(a.GetAccountInfo());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
